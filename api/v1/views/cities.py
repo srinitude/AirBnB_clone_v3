a30926aa6002ca660d1ab81689577380537b3b7c
Tope <tope628@gmail.com>
2017-12-15 14:18:00 +0000

    order of errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -68,16 +68,20 @@ def all_places(city_id):
 @cities.route("/<string:city_id>/places", methods=['POST'])
 def create_place(city_id):
     """Create a new place"""
+    if not storage.get("City", city_id):
+        abort(404)
     if not request.is_json:
         abort(400, "Not a JSON")
     new_place = request.get_json()
     if new_place.get("user_id") is None:
         abort(400, "Missing user_id")
-    user_id = new_place.get("user_id")
+    if not storage.get("User", user_id):
+        abort(404)
     if new_place.get("name") is None:
         abort(400, "Missing name")
     matching_city = storage.get("City", city_id)
     matching_user = storage.get("User", user_id)
+    user_id = new_place.get("user_id")
     if matching_city and matching_user:
         for key, val in new_place.items():
                 setattr(matching_city, key, val)
