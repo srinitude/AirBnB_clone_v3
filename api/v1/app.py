813eca0b69219f0807bf5488af46867234e304d8
Kiren <srinitude@gmail.com>
2017-12-19 23:29:37 +0000

    Adds support for /status endpoint

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,20 +2,21 @@
 from flask import Flask, Blueprint
 from models import storage
 from api.v1.views import app_views
+import os
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
+
 
 @app.teardown_appcontext
-def close():
+def close(f):
     """ app teardown """
     storage.close()
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    HBNB_HOST = os.getenv("HBNB_API_HOST", "0.0.0.0")
+    try:
+        HBNB_PORT = int(os.getenv("HBNB_API_PORT"))
+    except:
+        HBNB_PORT = 5000
+    app.run(host=HBNB_HOST, port=HBNB_PORT)
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+App Views Blueprint
+"""
+from flask import Blueprint
+import json
+
+app_views = Blueprint("app_views", __name__)
+
+from api.v1.views.index import *
