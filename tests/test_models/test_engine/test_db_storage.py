8640e64eeaf9e48beaa5118ce7e2ae0c2c40d1ed
Kiren <srinitude@gmail.com>
2017-12-20 23:39:54 +0000

    Fixes some db-storage related errors

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,39 +18,44 @@ import os
 import pep8
 import unittest
 import sqlalchemy
-FileStorage = file_storage.FileStorage
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+import models
+
+if models.storage == "db":
+        classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
            "Place": Place, "Review": Review, "State": State, "User": User}
 
+        class TestDBStorage(unittest.TestCase):
+	    """ tests for db storage """
+            def setUp(self):
+                self.session = db_storage.DBStorage()
+                self.session.reload()
 
-class TestDBStorage(unittest.TestCase):
-    """ tests for db storage """
-    def setUp(self):
-        self.session = db_storage.DBStorage()
+	    def tearDown(self):
+		self.session.close()
 
-    def tearDown(self):
-        self.session.close()
+	    def test_get_returns_none(self):
+		"""Test that get with nothing in it properly returns None"""
+		result = self.session.get("User", "234htnfjkegr42--23q524yhwgeasgset")
+		self.assertEqual(result, None)
 
-    def test_get_returns_none(self):
-        """Test that get with nothing in it properly returns None"""
-        result = self.session.get("User", "234htnfjkegr42--23q524yhwgeasgset")
-        self.assertEqual(result, None)
+	    def test_count_returns_none(self):
+		"""Test that count with nothing in it properly returns 0"""
+		result = self.session.count("BadClass")
+		self.assertEqual(result, 0)
 
-    def test_count_returns_none(self):
-        """Test that count with nothing in it properly returns 0"""
-        result = self.session.count()
-        self.assertEqual(result, 0)
+	    def test_valid_get(self):
+		"""Tests valid get"""
+		new_state = State(name="Montana")
+		self.session.new(new_state)
+		state_result = self.session.get("State", new_state.id)
+		self.assertEqual(id(new_state), id(state_result))
+		self.session.delete(new_state)
 
-    def test_valid_get(self):
-        """Tests valid get"""
-        new_user = User()
-        self.session.new(new_user)
-        user_result = self.session.get("User", new_user.id)
-        self.assertEqual(id(new_user), id(user_result))
-
-    def test_valid_count(self):
-        """Tests valid count"""
-        new_user = User()
-        self.session.new(new_user)
-        count = self.session.count("User")
-        self.assertEqual(count, 1)
+	    def test_valid_count(self):
+		"""Tests valid count"""
+		current_count = self.session.count("State")
+		new_state = State(name="Montana")
+		self.session.new(new_state)
+		new_count = self.session.count("State")
+		self.assertEqual(current_count + 1, new_count)
+		self.session.delete(new_state)
